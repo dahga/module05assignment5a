f5b79cdc40a9deaaef8c3afa68b4d86bdf3c57fb
nd0821wj <nadinfaranso14@gmail.com>
2022-05-03 15:57:29 -0500

    largest added

--- display.php
+++ display.php
@@ -16,7 +16,7 @@
       showButtons($name);
       threeDie();
    }
-   elseif ($button=="Roll Two"){
+   if ($button=="Roll Two"){
       showButtons($name);
       twoDie();
    }
@@ -39,7 +39,35 @@ function showButtons(&$name)
       <input type="hidden" name="name" value="$name">
 HERE;
 }
+/*
+function db()
+{
+if (!( $database = mysqli_connect( "localhost", "nadin-qaqos", "nNFBMMMQ13","nadin-qaqos_dice"))))
+      die( "Could not connect to database" );
 
+    $query = "INSERT INTO score(first, second, third, total) VALUES (";
+    $query = $query . "'$name','$owner','$species','$sex','$birth','$death')";
+   
+   
+   echo "<h4>query: $query</h4>"; 
+   if ( !( $result = mysqli_query( $database,$query ) ) ) {
+      echo "Could not execute query! <br />";
+      die( mysqli_error() );
+   }
+   else 
+      echo "$name added to pet database.";
+   mysqli_close($database);
+   echo <<< HERE
+   <form method="post" action="example3.php">
+   <input type="submit" name="button" value="return">
+   <input type="hidden" name="username" value="$username">
+   <input type="hidden" name="password" value="$password">
+   <input type="hidden" name="dbname" value="$dbname">
+   </form>
+HERE;
+
+}
+*/
 function namePage()
 {
    echo <<< HERE
@@ -49,11 +77,6 @@ function namePage()
 HERE;
 }
 
-function showDie()
-{
-   $die = rand(1,6);
-   echo "<img src=\"die$die.jpg\">";
-}
 
 function showAllDice()
 {
@@ -69,7 +92,24 @@ function threeDie()
    echo "<img src=\"die$die2.jpg\">";
    $die3 = rand(1,6);
    echo "<img src=\"die$die3.jpg\">";
+   echo "<h1>the largest is " . biggestForThree($die1, $die2, $die3) . "</h1>";
 }
+
+function biggestForThree($num1,$num2,$num3)
+{
+    if ($num1 > $num2)
+        if ($num1 > $num3)
+            $largest = $num1;
+        else
+            $largest = $num3;
+    else
+        if ($num2 > $num3)
+            $largest = $num2;
+        else
+            $largest = $num3;
+    return $largest;
+}
+
 
 function twoDie() 
 {
@@ -77,9 +117,26 @@ function twoDie()
    echo "<img src=\"die$die1.jpg\">";
    $die2 = rand(1,6);
    echo "<img src=\"die$die2.jpg\">";
+   echo "<h1>the largest is " . biggestForTwo($die1, $die2) . "</h1>";
    
 }
 
+function biggestForTwo($num1,$num2)
+{
+    if ($num1 > $num2)
+        $largest = $num1;
+    else
+        $largest = $num2;
+    
+    return $largest;
+}
+
+function showDie()
+{
+   $die1 = rand(1,6);
+   echo "<img src=\"die$die1.jpg\">";
+   echo "<h1>the largest is " . $die1 . "</h1>";
+}
 
 ?>
 </form>
