4d495fd361d111b710ecb327ca2214dfcdf09ba0
Dahga Muhumed <dahga@icloud.com>
2022-05-04 00:57:43 -0500

    currently have done- Dahga

--- display.php
+++ display.php
@@ -1,23 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+<a href="http://puff.mnstate.edu/~jn6160mv/private/"> Home </a> <br>
+
+<style>
+mark{
+    background-color:pink;
+    color: white;}
+body {
+    font-family: 'Courier New', monospace;
+    background-image: url('https://mocah.org/thumbs/1104173-sports-illustration-purple-dice-brand-EA-DICE-magenta-shape-number-recreation-games-font-indoor-games-and-sports-diagram-board-game-dice-game-tabletop-game.jpg');
+    }
+</style>
+
+
 <form>
 <?php
    extract($_REQUEST);
    
    if ($button=="begin")
       showButtons($name);
+     
+ 
    elseif ($button=="Roll One"){
       showButtons($name);
+     
       showDie();
    }
-   elseif ($button=="show all dice"){
+   /*elseif ($button=="show all dice"){
       showButtons($name);
       showAllDice();
-   }
+   }*/
    elseif ($button=="Roll All Three"){
-      showButtons($name);
+      showButtons($name1);
+     
       threeDie();
    }
    if ($button=="Roll Two"){
-      showButtons($name);
+      showButtons($name1);
+     
       twoDie();
    }
 
@@ -29,62 +50,42 @@
 function showButtons(&$name)
 {
    echo <<< HERE
-      <h1>Welcome $name! Lets get Rollin</h1>
+      <h1>Welcome $name1! Lets get Rollin </h1>
       <h2>Roll all three die first.</h2>
       <input type="submit" name="button" value="Roll All Three">
       <input type="submit" name="button" value="Roll Two">
       <input type="submit" name="button" value="Roll One"><br>
-      <input type="submit" name="button" value="show all dice">
+      <input type="hidden" name="text" value="show all dice">
       <input type="submit" name="button" value="Start Over">
+      <input type="hidden" name="name" value="$name">
       <input type="hidden" name="name" value="$name">
 HERE;
 }
-/*
-function db()
-{
-if (!( $database = mysqli_connect( "localhost", "nadin-qaqos", "??????","nadin-qaqos_dice"))))
-      die( "Could not connect to database" );
 
-    $query = "INSERT INTO score(first, second, third, total) VALUES (";
-    $query = $query . "'$name','$owner','$species','$sex','$birth','$death')";
-   
-   
-   echo "<h4>query: $query</h4>"; 
-   if ( !( $result = mysqli_query( $database,$query ) ) ) {
-      echo "Could not execute query! <br />";
-      die( mysqli_error() );
-   }
-   else 
-      echo "$name added to pet database.";
-   mysqli_close($database);
-   echo <<< HERE
-   <form method="post" action="example3.php">
-   <input type="submit" name="button" value="return">
-   <input type="hidden" name="username" value="$username">
-   <input type="hidden" name="password" value="$password">
-   <input type="hidden" name="dbname" value="$dbname">
-   </form>
-HERE;
 
-}
-*/
+
 function namePage()
 {
    echo <<< HERE
-   <h1>Enter Your Name</h1>
-   <h3>Name <input type="text" name="name" autocomplete="off"></h3>
+   <h1 style="text-align:center;">Are you ready to Go to Boston??</h1>
+   <h2 style="text-align:center;"> <i> Well lets get started! </i></h2>
+   <h3><mark> Here are some basic rules for the game before we get started!</mark></h3>
+   <a href= "rules.html"><h3>rules</h3></a>
+   <h4>Enter your name here!</h4>
+   <h5>Player 1: <input type="text" name="name" autocomplete="off"></h5>
+   <h5> Player 2: <input type="text" name="name" autocomplete="off"></h5>
    <input type="submit" name="button" value="begin">
 HERE;
 }
 
 
-function showAllDice()
+/*function showAllDice()
 {
    for ($i=1; $i<=6; $i++)
    echo "<img src=\"die$i.jpg\">";
-}
+}*/
 
-function threeDie() 
+function threeDie()
 {
    $die1 = rand(1,6);
    echo "<img src=\"die$die1.jpg\">";
@@ -92,7 +93,7 @@ function threeDie()
    echo "<img src=\"die$die2.jpg\">";
    $die3 = rand(1,6);
    echo "<img src=\"die$die3.jpg\">";
-   echo "<h1>the largest is " . biggestForThree($die1, $die2, $die3) . "</h1>";
+   echo "<h1>the largest die is: " . biggestForThree($die1, $die2, $die3) . "</h1>";
 }
 
 function biggestForThree($num1,$num2,$num3)
@@ -111,13 +112,13 @@ function biggestForThree($num1,$num2,$num3)
 }
 
 
-function twoDie() 
+function twoDie()
 {
    $die1 = rand(1,6);
    echo "<img src=\"die$die1.jpg\">";
    $die2 = rand(1,6);
    echo "<img src=\"die$die2.jpg\">";
-   echo "<h1>the largest is " . biggestForTwo($die1, $die2) . "</h1>";
+   echo "<h1>the largest die is: " . biggestForTwo($die1, $die2) . "</h1>";
    
 }
 
@@ -127,7 +128,7 @@ function biggestForTwo($num1,$num2)
         $largest = $num1;
     else
         $largest = $num2;
-    
+   
     return $largest;
 }
 
@@ -138,5 +139,8 @@ function showDie()
    echo "<h1>the largest is " . $die1 . "</h1>";
 }
 
+
+
 ?>
 </form>
+
